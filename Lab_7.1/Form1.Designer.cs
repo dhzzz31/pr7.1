7019c3b67459671327d4dec037787c1c2c405eff
Seniy_Jalilow Seniy_Jalilow <arseniy-3108@mail.ru>
2023-11-09 22:03:25 +0500

    Добавьте файлы проекта.

---
+++ Lab_7.1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lab_7._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.file1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Location = new System.Drawing.Point(50, 37);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(200, 200);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.AutoSize = true;
+            this.btn.Location = new System.Drawing.Point(189, 256);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(113, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Загрузить рисунок";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // file1
+            // 
+            this.file1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(12, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сохранить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 291);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pct);
+            this.Name = "Form1";
+            this.Text = "Просмотр графического файла";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.OpenFileDialog file1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Lab_7.1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // задаем фильтр для файлов только .jpg
+            // до слеша как отображается фильтр в окне, а после сама маска
+            file1.Filter = "(*.jpg)|*.jpg";
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            // создаем переменную fname строкового типа
+            string fname;
+            // открываем Проводник
+            file1.ShowDialog();
+            // используем переменную для хранения имени выбранного файла
+            fname = file1.FileName;
+            // зпгружаем файл в PictureBox
+            pct.Image = Image.FromFile(fname);
+            // в textbox1 выводится полное имя открытого файла после нажатия ок
+            if (file1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = file1.SafeFileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Проверяем, содержит ли элемент pct изображение
+            if (pct.Image != null)
+            {
+                // Создаем объект диалогового окна для сохранения файла
+                SaveFileDialog sfg = new SaveFileDialog();
+                // Устанавливаем заголовок диалогового окна как текст элемента pct
+                sfg.Title = pct.Text;
+                // Устанавливаем заголовок диалогового окна как текст элемента pct
+                sfg.OverwritePrompt = true;
+                // Проверять существование пути к файлу
+                sfg.CheckPathExists = true;
+                // Задаем фильтр для выбора типа файла (только jpg)
+                sfg.Filter = "(*.jpg)|*.jpg";
+                // Показывает справку в диалоговом окне
+                sfg.ShowHelp = true;
+                // Проверяем, выбран ли файл в диалоговом окне
+                if (sfg.ShowDialog() == DialogResult.OK)
+                {
+                    // Сохраняем изображение элемента pct в выбранный файл
+                    pct.Image.Save(sfg.FileName);
+                }
+            }
+        }
+    }
+}
